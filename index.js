3ba451a4e121916f43b69b484cc2fce7f1f0e8dc
ViktorKonsta <viktor.konsta@gmail.com>
2016-11-13 23:21:03 +0200

    Add fast-json-parse and apply this instead of JSON.parse

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var pkg = require("./package.json")
 var MongoClient = require("mongodb").MongoClient
 var params = require("commander")
 var stringify = require("fast-safe-stringify")
+var jsonParse = require("fast-json-parse")
 
 params
   .version(pkg.version)
@@ -45,7 +46,7 @@ function onConnection(e, db) {
     var document = null
 
     try {
-      document = JSON.parse(data)
+      document = jsonParse(data)
     } catch (e) {
       document = {
         msg: data
