f99a2245107a374855e05f7701ed93938fbe257d
ViktorKonsta <viktor.konsta@gmail.com>
2016-11-27 22:17:07 +0200

    Rename params to program as for commander it's more convinient, change stdin

--- pino-mongodb.js
+++ pino-mongodb.js
@@ -4,9 +4,9 @@
 const pkg = require('./package.json')
 const MongoClient = require('mongodb').MongoClient
 const readline = require('readline')
-const params = require('commander')
+const program = require('commander')
 
-params
+program
   .version(pkg.version)
   .description(pkg.description)
   .option('-H, --host <address>', 'set database host (localhost)', 'localhost')
@@ -19,12 +19,12 @@ params
   .option('--show-insert-errors', 'show errors from inserting documents into mongodb (true)', true)
   .parse(process.argv)
 
-params.host = process.env.DB_HOST || params.host
-params.port = process.env.DB_PORT || params.port
-params.db = process.env.DB_NAME || params.db
-params.collection = process.env.DB_COLLECTION || params.collection
-params.user = process.env.DB_USER || params.user
-params.pass = process.env.DB_PASS || params.pass
+program.host = process.env.DB_HOST || program.host
+program.port = process.env.DB_PORT || program.port
+program.db = process.env.DB_NAME || program.db
+program.collection = process.env.DB_COLLECTION || program.collection
+program.user = process.env.DB_USER || program.user
+program.pass = process.env.DB_PASS || program.pass
 
 if (require.main === module) {
   main()
@@ -34,10 +34,10 @@ function main () {
   const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
-    terminal: !params.quiet
+    terminal: !program.quiet
   })
 
-  MongoClient.connect(makeMongoOptions(params), function onConnection (e, db) {
+  MongoClient.connect(makeMongoOptions(program), function onConnection (e, db) {
     if (e) {
       return handleError(e)
     }
@@ -49,38 +49,37 @@ function main () {
     })
 
     rl.on('line', stdin.bind({
-      collection: db.collection(params.collection)
+      collection: db.collection(program.collection)
     }))
   })
 }
 
 function stdin (data) {
   const jsonParse = this.jsonParse || require('fast-json-parse')
-  const params = this.params || params
-  const json = jsonParse(data).value
-  let document = {}
+  const program = this.program || program
+  let log = jsonParse(data)
 
-  if (json) {
-    document = json
+  if (log.value) {
+    log = log.value
   } else {
-    document = {
+    log = {
       msg: data
     }
   }
 
-  this.collection.insertOne(document, function insertOne (e) {
-    if (e && params.showInsertErrors) {
+  this.collection.insertOne(log, function insertOne (e) {
+    if (e && program.showInsertErrors) {
       return handleError(e)
     }
   })
 }
 
-function makeMongoOptions (params) {
+function makeMongoOptions (program) {
   let string = 'mongodb://'
-  if (params.user && params.pass) {
-    string += '[' + params.user + ':' + params.pass + ']@'
+  if (program.user && program.pass) {
+    string += '[' + program.user + ':' + program.pass + ']@'
   }
-  string += params.host + ':' + params.port + '/' + params.db
+  string += program.host + ':' + program.port + '/' + program.db
   return string
 }
 
