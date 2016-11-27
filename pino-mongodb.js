ca6fad5857b8df60668e85a795c2b3c337e21d0a
ViktorKonsta <viktor.konsta@gmail.com>
2016-11-27 21:06:26 +0200

    Add readline to handle stdin and stdout, add quiet option

--- pino-mongodb.js
+++ pino-mongodb.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 var pkg = require('./package.json')
 var MongoClient = require('mongodb').MongoClient
+var readline = require('readline')
 var params = require('commander')
-var jsonParse = require('fast-json-parse')
 
 params
   .version(pkg.version)
@@ -13,7 +13,14 @@ params
   .option('-c, --collection <name>', 'DataBase collection name (logs)', 'logs')
   .option('-u, --user <username>', 'DataBase username (root)', 'root')
   .option('-p, --pass <password>', 'DataBase password (null)', null)
+  .option('-q, --quiet', 'Suppress output', false)
   .parse(process.argv)
+
+var rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  terminal: !params.quiet
+})
 
 params.host = process.env.DB_HOST || params.host
 params.port = process.env.DB_PORT || params.port
@@ -22,10 +29,7 @@ params.collection = process.env.DB_COLLECTION || params.collection
 params.user = process.env.DB_USER || params.user
 params.pass = process.env.DB_PASS || params.pass
 
-MongoClient.connect(makeMongoOptions(params), onConnection)
-process.stdin.pipe(process.stdout)
-
-function onConnection (e, db) {
+MongoClient.connect(makeMongoOptions(params), function onConnection (e, db) {
   if (e) {
     return handleError(e)
   }
@@ -38,32 +42,27 @@ function onConnection (e, db) {
     })
   })
 
-  process.stdin.on('data', function stdin (data) {
-    var document = {}
-    var json = jsonParse(data).value
+  rl.on('line', stdin.bind({ collection: collection }))
+})
 
-    if (json) {
-      document = json
-    } else {
-      document = {
-        msg: data
-      }
-    }
+function stdin (data) {
+  var document = {}
+  var jsonParse = this.jsonParse || require('fast-json-parse')
+  var json = jsonParse(data).value
 
-    collection.insertOne(document, function insertOne (e) {
-      if (e) {
-        return handleError(e)
-      }
-    })
-  })
-}
-
-function handleError (e) {
-  if (e instanceof Error) {
-    console.error(e)
+  if (json) {
+    document = json
   } else {
-    console.error(new Error(e))
+    document = {
+      msg: data
+    }
   }
+
+  this.collection.insertOne(document, function insertOne (e) {
+    if (e) {
+      return handleError(e)
+    }
+  })
 }
 
 function makeMongoOptions (params) {
@@ -77,8 +76,16 @@ function makeMongoOptions (params) {
   return string
 }
 
+function handleError (e) {
+  if (e instanceof Error) {
+    console.error(e)
+  } else {
+    console.error(new Error(e))
+  }
+}
+
 module.exports = {
-  onConnection: onConnection,
   handleError: handleError,
-  makeMongoOptions: makeMongoOptions
+  makeMongoOptions: makeMongoOptions,
+  stdin: stdin
 }
