06c61489a57481a012ebda5d6db87d553ea29def
Manuel Spigolon <manuel.spigolon@nearform.com>
2021-09-13 16:16:44 +0200

    more tests

--- test/end-to-end/transport.js
+++ test/end-to-end/transport.js
@@ -48,3 +48,35 @@ t.test('auth transport test', async (t) => {
   const rowsAfter = await collection.countDocuments()
   t.equal(rowsAfter, rowsBefore + 3, 'logged 3 rows')
 })
+
+t.test('auth transport test', async (t) => {
+  const options = {
+    uri: 'mongodb://one:two@localhost:27017/dbname?authSource=admin',
+    collection: 'log-test'
+  }
+
+  const client = new MongoClient(options.uri, options.mongoOptions)
+  await client.connect()
+  t.teardown(client.close.bind(client))
+  const db = client.db(options.database)
+  const collection = db.collection(options.collection)
+
+  const rowsBefore = await collection.countDocuments()
+
+  const transport = pino.transport({
+    target: '../../pino-mongodb.js',
+    level: 'info',
+    options
+  })
+  const log = pino(transport)
+  await once(transport, 'ready')
+  log.info('this is a string log')
+  log.debug('ignored')
+  log.info('this is a long string log'.repeat(1000))
+  log.fatal(new Error('ops'), 'not ignored')
+  t.pass('logged on mongo')
+
+  await setTimeout(1000)
+  const rowsAfter = await collection.countDocuments()
+  t.equal(rowsAfter, rowsBefore + 3, 'logged 3 rows')
+})
