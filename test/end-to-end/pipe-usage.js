a1f6990d45764e46b7e3347a9386d25f8958e8db
Tarrask <damien.plumettaz@plumettaz.com>
2022-06-30 12:12:15 +0200

    fix: pipe option -o (#118)
    * build(deps): bump actions/dependency-review-action from 1 to 2 (#2)
    
    Bumps [actions/dependency-review-action](https://github.com/actions/dependency-review-action) from 1 to 2.
    - [Release notes](https://github.com/actions/dependency-review-action/releases)
    - [Commits](https://github.com/actions/dependency-review-action/compare/v1...v2)
    
    ---
    updated-dependencies:
    - dependency-name: actions/dependency-review-action
      dependency-type: direct:production
      update-type: version-update:semver-major
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * fix: pipe option -o
    
    This is a fix for issue #76.
    In mongodb 4, the result object returned by insertOne does not contain the ops array anymore.
    
    This fix use the original log object of the insert function to output it on the console when the -o option is defined
    
    * test: simplifies test so it pass more consistently
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- test/end-to-end/pipe-usage.js
+++ test/end-to-end/pipe-usage.js
@@ -5,6 +5,7 @@ const { spawn } = require('child_process')
 const { promisify } = require('util')
 const { MongoClient } = require('mongodb')
 const { once } = require('events')
+const EOL = require('os').EOL
 
 const mongoUrl = 'mongodb://one:two@localhost:27017/newdb?authSource=admin'
 const setTimeout = promisify(global.setTimeout)
@@ -36,6 +37,51 @@ t.test('must log to a custom collection', async (t) => {
 
   await setTimeout(5000)
   childProcess.kill('SIGINT')
+  try {
+    await once(childProcess, 'close')
+    const rowsAfter = await collection.countDocuments()
+    t.equal(rowsAfter, rowsBefore + 3, 'logged 3 rows')
+  } catch (error) {
+    t.error(error)
+  }
+})
+
+t.test('must write logs to the console with -o option', async (t) => {
+  const customCollection = 'custom-collection'
+  const childProcess = spawn('node', [
+    '../../pino-mongodb.js',
+    mongoUrl,
+    '-o',
+    '-c',
+    customCollection
+  ], {
+    cwd: __dirname,
+    stdio: ['pipe', 'pipe', process.stderr]
+  })
+
+  const client = new MongoClient(mongoUrl)
+  await client.connect()
+  t.teardown(client.close.bind(client))
+  const db = client.db()
+  const collection = db.collection(customCollection)
+
+  const rowsBefore = await collection.countDocuments()
+  t.pass(`rows count ${rowsBefore}`)
+
+  childProcess.stdin.write('hello pino-mongo 1\n')
+  childProcess.stdin.write(`${JSON.stringify({ hello: 'pino' })}\n`)
+  childProcess.stdin.write('hello pino-mongo 2\n')
+  await setTimeout(5000)
+
+  childProcess.kill('SIGINT')
+  // read stdout
+  const chunks = []
+  for await (let chunk of childProcess.stdout) {
+    chunks.push(chunk)
+  }
+  const output = Buffer.concat(chunks).toString()
+  t.equal(output.trim().split('\n').length, 3)
+
   try {
     await once(childProcess, 'close')
     const rowsAfter = await collection.countDocuments()
